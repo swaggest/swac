12b43a4c9b1b128df79715c0ffa06cece087af4b
Viacheslav Poturaev <vearutop@gmail.com>
2020-08-30 11:45:53 +0200

    Add symfony/yaml to non-dev deps

--- src/Command/App.php
+++ src/Command/App.php
@@ -7,7 +7,7 @@ use Yaoi\Command\Definition;
 
 class App extends Command\Application
 {
-    static $ver = 'v0.1.15';
+    static $ver = 'v0.1.16';
 
     public $phpGuzzleClient;
     public $goClient;
