1f9b4452e941122db94ec9f9d000db73c813f39e
Viacheslav Poturaev <vearutop@gmail.com>
2022-09-20 15:03:36 +0200

    Update deps

--- src/Command/App.php
+++ src/Command/App.php
@@ -7,7 +7,7 @@ use Yaoi\Command\Definition;
 
 class App extends Command\Application
 {
-    static $ver = 'v0.1.30';
+    static $ver = 'v0.1.31';
 
     public $phpGuzzleClient;
     public $goClient;
