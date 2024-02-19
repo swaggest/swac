b06a2d4e68d96c1758934bcbd43bc2f7fc39aa7f
Viacheslav Poturaev <vearutop@gmail.com>
2024-02-19 18:36:15 +0100

    Bump app version

--- src/Command/App.php
+++ src/Command/App.php
@@ -7,7 +7,7 @@ use Yaoi\Command\Definition;
 
 class App extends Command\Application
 {
-    static $ver = 'v0.1.34';
+    static $ver = 'v0.1.35';
 
     public $phpGuzzleClient;
     public $goClient;
